e88f29eec73e82250c198a28afc7ea5273348f41
Vito <lolozol80xd@gmail.com>
2023-09-26 11:47:35 +0200

    finish

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Employee emp = new Employee();
+        emp.name = "Tomi";
+        emp.city = "Aszód";
+        emp.salary = 385;
+        emp.status = Status.ACTIVE;
+        if(emp.status == Status.ACTIVE){
+            System.out.println("Aztakurvaaaaaaaaaa");
+        }
     }
 }
